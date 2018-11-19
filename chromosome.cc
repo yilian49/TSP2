3db6eff3c8916ea2f22cbb6289da20e39699ff6a
yilian <liub@reed.edu>
2018-11-19 15:44:32 -0800

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	new file:   challenge.tsv
    	new file:   chromosome.cc
    	new file:   chromosome.hh
    	new file:   cities.cc
    	new file:   cities.hh
    	new file:   deme.cc
    	new file:   deme.hh
    	new file:   five.tsv
    	new file:   linear.tsv
    	new file:   shortest.tsv
    	new file:   tsp.cc

---
+++ chromosome.cc
@@ -0,0 +1,105 @@
+/*
+ * Implementation for Chromosome class
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <random>
+
+#include "chromosome.hh"
+
+//////////////////////////////////////////////////////////////////////////////
+// Generate a completely random permutation from a list of cities
+Chromosome::Chromosome(const Cities* cities_ptr)
+  : cities_ptr_(cities_ptr),
+    order_(random_permutation(cities_ptr->size()))
+{
+  assert(is_valid());
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Clean up as necessary
+Chromosome::~Chromosome()
+{
+  assert(is_valid());
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Perform a single mutation on this chromosome
+void
+Chromosome::mutate()
+{
+  // Add your implementation here
+
+  assert(is_valid());
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Return a pair of offsprings by recombining with another chromosome
+// Note: this method allocates memory for the new offsprings
+std::pair<Chromosome*, Chromosome*>
+Chromosome::recombine(const Chromosome* other)
+{
+  assert(is_valid());
+  assert(other->is_valid());
+
+  // Add your implementation here
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// For an ordered set of parents, return a child using the ordered crossover.
+// The child will have the same values as p1 in the range [b,e),
+// and all the other values in the same order as in p2.
+Chromosome*
+Chromosome::create_crossover_child(const Chromosome* p1, const Chromosome* p2,
+                                   unsigned b, unsigned e) const
+{
+  Chromosome* child = p1->clone();
+
+  // We iterate over both parents separately, copying from parent1 if the
+  // value is within [b,e) and from parent2 otherwise
+  unsigned i = 0, j = 0;
+
+  for ( ; i < p1->order_.size() && j < p2->order_.size(); ++i) {
+    if (i >= b and i < e) {
+      child->order_[i] = p1->order_[i];
+    }
+    else { // Increment j as long as its value is in the [b,e) range of p1
+      while (p1->is_in_range(p2->order_[j], b, e)) {
+        ++j;
+      }
+      assert(j < p2->order_.size());
+      child->order_[i] = p2->order_[j];
+      j++;
+    }
+  }
+
+  assert(child->is_valid());
+  return child;
+}
+
+// Return a positive fitness value, with higher numbers representing
+// fitter solutions (shorter total-city traversal path).
+double
+Chromosome::get_fitness() const
+{
+  // Add your implementation here
+}
+
+// A chromsome is valid if it has no repeated values in its permutation,
+// as well as no indices above the range (length) of the chromosome.
+// We implement this check with a sort, which is a bit inefficient, but simple
+bool
+Chromosome::is_valid() const
+{
+  // Add your implementation here
+}
+
+// Find whether a certain value appears in a given range of the chromosome.
+// Returns true if value is within the specified the range specified
+// [begin, end) and false otherwise.
+bool
+Chromosome::is_in_range(unsigned value, unsigned begin, unsigned end) const
+{
+  // Add your implementation here
+}
---
+++ deme.cc
@@ -0,0 +1,45 @@
+/*
+ * Declarations for Deme class to evolve a genetic algorithm for the
+ * travelling-salesperson problem.  A deme is a population of individuals.
+ */
+
+#include "chromosome.hh"
+#include "deme.hh"
+
+// Generate a Deme of the specified size with all-random chromosomes.
+// Also receives a mutation rate in the range [0-1].
+Deme::Deme(const Cities* cities_ptr, unsigned pop_size, double mut_rate)
+{
+  // Add your implementation here
+}
+
+// Clean up as necessary
+Deme::~Deme()
+{
+  // Add your implementation here
+}
+
+// Evolve a single generation of new chromosomes, as follows:
+// We select pop_size/2 pairs of chromosomes (using the select() method below).
+// Each chromosome in the pair can be randomly selected for mutation, with
+// probability mut_rate, in which case it calls the chromosome mutate() method.
+// Then, the pair is recombined once (using the recombine() method) to generate
+// a new pair of chromosomes, which are stored in the Deme.
+// After we've generated pop_size new chromosomes, we delete all the old ones.
+void Deme::compute_next_generation()
+{
+  // Add your implementation here
+}
+
+// Return a copy of the chromosome with the highest fitness.
+const Chromosome* Deme::get_best() const
+{
+  // Add your implementation here
+}
+
+// Randomly select a chromosome in the population based on fitness and
+// return a pointer to that chromosome.
+Chromosome* Deme::select_parent()
+{
+  // Add your implementation here
+}
